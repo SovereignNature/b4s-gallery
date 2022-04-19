fded7cb184359a82a72354796d9449fdb1c989e4
GiovanneGuerra <giovanneguerra@hotmail.com>
2022-04-19 19:54:02 +0100

    feature: Make load async and block button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { filter } from 'rxjs/operators';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { OpenseaGalleryService } from '@app/services/opensea-gallery.service';
 import { Breath } from '@app/interfaces/breath';
 import { Metadata } from '@app/interfaces/metadata';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +12,10 @@ import { Metadata } from '@app/interfaces/metadata';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'bfs-gallery';
   openSeaUrl!: string;
   breath!: Breath;
   metadata!: Metadata;
-  paramsObject!: Params;
-  loader = false;
+  image$!: Observable<boolean>;
   tid!: string;
   cid!: string;
 
@@ -29,19 +26,22 @@ export class AppComponent implements OnInit {
       this.cid = params.cid;
       this.tid = params.tid;
       this.openSeaUrl = `https://testnets.opensea.io/assets/0xd653694558af69d09709768afac9e35c9fb984c8/${this.tid}/?force_update=true`;
+      
       if(this.cid !== undefined && this.tid !== undefined) {
+
         this.openSeaService.getBreath(this.cid, this.tid).subscribe((value: Breath) => {
           this.breath = value;
         });
-
+        
         this.openSeaService.getMetadata(this.cid, this.tid).subscribe((value: Metadata) => {
           this.metadata = value;
         });
+
+        this.openSeaService.getImage(this.cid, this.tid).subscribe((value: Blob) => {
+          this.image$ = value ? of(true) : of(false);
+        });
+
       }
     });
-  }
-
-  handleMissingImage(event: Event): void {    
-    event.type === 'error' ?  this.loader = true : this.loader = false;
   }
  }
--- src/app/services/opensea-gallery.service.ts
+++ src/app/services/opensea-gallery.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders,  } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 import { Breath } from '@app/interfaces/breath';
 import { Metadata } from '@app/interfaces/metadata';
 import { environment } from '@env/environment';
@@ -10,7 +11,6 @@ import { environment } from '@env/environment';
   providedIn: 'root'
 })
 export class OpenseaGalleryService {
-
   constructor(private http: HttpClient) { }
 
   getBreath(collection: string, id: string): Observable<Breath> {    
@@ -19,5 +19,17 @@ export class OpenseaGalleryService {
 
   getMetadata(collection: string, id: string): Observable<Metadata> {
     return this.http.get<Metadata>(`${environment.apiUrl}${collection}/metadata/${id}`);
+  }
+
+  getImage(collection: string, id: string): Observable<any> {
+    console.log(`${environment.apiUrl}${collection}/images/${id}.png`);
+    const httpOptions: Object = {
+      headers: new HttpHeaders({
+        'Accept': 'image/png',
+      }),
+      responseType: 'blob'
+    };
+    
+    return this.http.get<Observable<Blob>>(`${environment.apiUrl}${collection}/images/${id}.png`, httpOptions);
   }
 }

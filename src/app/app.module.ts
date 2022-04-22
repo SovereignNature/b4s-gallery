32d8d1e90269f35c316f82093e3626c8f40af2fe
GiovanneGuerra <giovanneguerra@hotmail.com>
2022-04-22 13:19:32 +0100

    chore: Deploy second version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProgressbarComponent } from './components/progressbar/progressbar.component';
-import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProgressbarComponent,
-    ModalComponent
+    ProgressbarComponent
   ],
   imports: [
     BrowserModule,

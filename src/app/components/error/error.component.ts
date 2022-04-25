ffb59d561f73b445ef00215b333836cec7028efa
GiovanneGuerra <giovanneguerra@hotmail.com>
2022-04-25 12:23:25 +0100

    feature: Create error page

---
+++ src/app/components/error/error.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bfs-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.scss']
+})
+export class ErrorComponent {}
